f6c60b5f0c8d2d0386f7b4525d0610f2afe6bca6
Christopher M. Baker <mail.chris.baker@gmail.com>
2013-10-17 07:38:00 -0400

    Refactored the timer test to make it easier to read.

--- tests/e3-timer-test.c
+++ tests/e3-timer-test.c
@@ -24,36 +24,37 @@
 #include "e3-timer-test.h"
 #include <e3.h>
 
-typedef struct test_cookie {
-    e3_timer_t timers[2];
+typedef struct test {
+    e3_timer_t timer_1;
+    e3_timer_t timer_2;
     unsigned int counter;
-} test_cookie_t;
+} test_t;
 
 static void
-increment(test_cookie_t *cookie) {
-    cookie->counter++;
+increment(test_t *test) {
+    test->counter++;
 }
 
 static void
-triple(test_cookie_t *cookie) {
-    cookie->counter *= 3;
+triple(test_t *test) {
+    test->counter *= 3;
 }
 
 static void
-reschedule(test_cookie_t *cookie) {
-    cookie->counter++;
+reschedule(test_t *test) {
+    test->counter++;
     
-    e3_timer_create(cookie->timers, 0,
-        (e3_timer_function_t) reschedule, cookie);
+    e3_timer_create(&test->timer_1, 0,
+        (e3_timer_function_t) reschedule, test);
 }
 
 void
 e3_timer_test(jasmine_t *jasmine) {
-    test_cookie_t cookie;
+    test_t test;
 
     jasmine_describe(jasmine, "the system timer") {
         jasmine_before(jasmine) {
-            cookie.counter = 0;
+            test.counter = 0;
         }
 
         jasmine_after(jasmine) {
@@ -62,87 +63,87 @@ e3_timer_test(jasmine_t *jasmine) {
 
         jasmine_it(jasmine, "can tick without any timers created") {
             jasmine_expect(jasmine, !e3_timer_tick());
-            jasmine_expect(jasmine, cookie.counter == 0);
+            jasmine_expect(jasmine, test.counter == 0);
         }
         
         jasmine_it(jasmine, "does not require an interrupt for a zero tick") {
-            e3_timer_create(cookie.timers, 0,
-                (e3_timer_function_t) increment, &cookie);
+            e3_timer_create(&test.timer_1, 0,
+                (e3_timer_function_t) increment, &test);
 
-            jasmine_expect(jasmine, cookie.counter == 0);
+            jasmine_expect(jasmine, test.counter == 0);
             jasmine_expect(jasmine, !e3_timer_tick());
-            jasmine_expect(jasmine, cookie.counter == 1);
+            jasmine_expect(jasmine, test.counter == 1);
         }
         
         jasmine_it(jasmine, "can have multiple timers with the same ticks") {
-            e3_timer_create(cookie.timers, 0,
-                (e3_timer_function_t) increment, &cookie);
+            e3_timer_create(&test.timer_1, 0,
+                (e3_timer_function_t) increment, &test);
                 
-            e3_timer_create(cookie.timers + 1, 0,
-                (e3_timer_function_t) increment, &cookie);
+            e3_timer_create(&test.timer_2, 0,
+                (e3_timer_function_t) increment, &test);
                 
-            jasmine_expect(jasmine, cookie.counter == 0);
+            jasmine_expect(jasmine, test.counter == 0);
             jasmine_expect(jasmine, !e3_timer_tick());
-            jasmine_expect(jasmine, cookie.counter == 2);
+            jasmine_expect(jasmine, test.counter == 2);
         }
         
         jasmine_it(jasmine, "can delete a timer") {
-            e3_timer_create(cookie.timers, 0,
-                (e3_timer_function_t) triple, &cookie);
+            e3_timer_create(&test.timer_1, 0,
+                (e3_timer_function_t) triple, &test);
                 
-            e3_timer_create(cookie.timers + 1,
-                0, (e3_timer_function_t) increment, &cookie);
+            e3_timer_create(&test.timer_2,
+                0, (e3_timer_function_t) increment, &test);
                 
-            jasmine_expect(jasmine, cookie.counter == 0);
-            e3_timer_delete(cookie.timers);
+            jasmine_expect(jasmine, test.counter == 0);
+            e3_timer_delete(&test.timer_1);
             jasmine_expect(jasmine, !e3_timer_tick());
-            jasmine_expect(jasmine, cookie.counter == 1);
+            jasmine_expect(jasmine, test.counter == 1);
         }
         
         jasmine_it(jasmine, "can tick faster than the interrupt") {
-            e3_timer_create(cookie.timers, 1,
-                (e3_timer_function_t) increment, &cookie);
+            e3_timer_create(&test.timer_1, 1,
+                (e3_timer_function_t) increment, &test);
                 
-            jasmine_expect(jasmine, cookie.counter == 0);
+            jasmine_expect(jasmine, test.counter == 0);
             jasmine_expect(jasmine, e3_timer_tick());
-            jasmine_expect(jasmine, cookie.counter == 0);
+            jasmine_expect(jasmine, test.counter == 0);
             jasmine_expect(jasmine, e3_timer_tick());
-            jasmine_expect(jasmine, cookie.counter == 0);
+            jasmine_expect(jasmine, test.counter == 0);
             e3_timer_interrupt();
-            jasmine_expect(jasmine, cookie.counter == 0);
+            jasmine_expect(jasmine, test.counter == 0);
             jasmine_expect(jasmine, !e3_timer_tick());
-            jasmine_expect(jasmine, cookie.counter == 1);
+            jasmine_expect(jasmine, test.counter == 1);
         }
         
         jasmine_it(jasmine, "can tick slower than the interrupt") {
-            e3_timer_create(cookie.timers, 2,
-                (e3_timer_function_t) triple, &cookie);
+            e3_timer_create(&test.timer_1, 2,
+                (e3_timer_function_t) triple, &test);
                 
-            e3_timer_create(cookie.timers + 1, 1,
-                (e3_timer_function_t) increment, &cookie);
+            e3_timer_create(&test.timer_2, 1,
+                (e3_timer_function_t) increment, &test);
                 
-            jasmine_expect(jasmine, cookie.counter == 0);
+            jasmine_expect(jasmine, test.counter == 0);
             e3_timer_interrupt();
             e3_timer_interrupt();
             e3_timer_interrupt();
             e3_timer_interrupt();
-            jasmine_expect(jasmine, cookie.counter == 0);
+            jasmine_expect(jasmine, test.counter == 0);
             jasmine_expect(jasmine, !e3_timer_tick());
-            jasmine_expect(jasmine, cookie.counter == 3);
+            jasmine_expect(jasmine, test.counter == 3);
         }
         
         jasmine_it(jasmine, "can create a timer within a timer function") {
-            e3_timer_create(cookie.timers, 0,
-                (e3_timer_function_t) reschedule, &cookie);
+            e3_timer_create(&test.timer_1, 0,
+                (e3_timer_function_t) reschedule, &test);
                 
-            jasmine_expect(jasmine, cookie.counter == 0);
+            jasmine_expect(jasmine, test.counter == 0);
             jasmine_expect(jasmine, e3_timer_tick());
-            jasmine_expect(jasmine, cookie.counter == 1);
+            jasmine_expect(jasmine, test.counter == 1);
             jasmine_expect(jasmine, e3_timer_tick());
-            jasmine_expect(jasmine, cookie.counter == 2);
-            e3_timer_delete(cookie.timers);
+            jasmine_expect(jasmine, test.counter == 2);
+            e3_timer_delete(&test.timer_1);
             jasmine_expect(jasmine, !e3_timer_tick());
-            jasmine_expect(jasmine, cookie.counter == 2);
+            jasmine_expect(jasmine, test.counter == 2);
         }
     }
 }
